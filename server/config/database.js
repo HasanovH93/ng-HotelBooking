2aec8c42707d52ce7d07bb1f3ea7ee591cf3a20b
HasanovH93 <hasanovh14@gmail.com>
2022-11-12 19:19:25 +0100

    add config

---
+++ server/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+// TODO change database according to assignment
+const CONNECTION_STRING = 'mongodb://localhost:27017/scaffoldDb'
+
+module.exports = async (app) => {
+   try {
+    await mongoose.connect(CONNECTION_STRING, {
+        useNewUrlParser: true,
+        UseUnifiedTopology: true
+    })
+    console.log("Database connected")
+   }catch(err){
+   console.error(err.message);
+   process.exit(1)
+   }
+};
