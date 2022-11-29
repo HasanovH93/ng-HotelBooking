92b2d5d84f86d34a0504a9f776e977eaf005aadd
HasanovH93 <hasanovh14@gmail.com>
2022-11-29 11:37:55 +0100

    some changes

--- server/middlewares/session.js
+++ server/middlewares/session.js
@@ -3,7 +3,7 @@ const { parseToken } = require("../services/user");
 module.exports = () => (req, res, next) => {
 
   
-    const token = req.headers["x-authorization"].split(' ')[1]
+    const token = req.headers["authorization"].split(' ')[1]
     console.log(token)
 
   if (token) {
