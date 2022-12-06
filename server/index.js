e164b769b06d1a51867832a16a7810adce280316
HasanovH93 <hasanovh14@gmail.com>
2022-12-06 12:11:10 +0100

    add bcryptjs

--- server/index.js
+++ server/index.js
@@ -1,22 +1,15 @@
-require('dotenv').config()
-const express = require('express');
-const expressConfig = require('./config/express');
-const databaseConfig = require('./config/database');
-const routerConfig = require('./config/routes');
-
-
+require("dotenv").config();
+const express = require("express");
+const expressConfig = require("./config/express");
+const databaseConfig = require("./config/database");
+const routerConfig = require("./config/routes");
 
 start();
-async function start(){
-      
-    const app = express();
-    expressConfig(app);
-    await databaseConfig(app);
-    routerConfig(app);
-    
+async function start() {
+  const app = express();
+  expressConfig(app);
+  await databaseConfig(app);
+  routerConfig(app);
 
- 
-    
-    
-    app.listen(process.env.PORT, () => console.log('REST Service started!'))
+  app.listen(process.env.PORT, () => console.log("REST Service started!"));
 }
